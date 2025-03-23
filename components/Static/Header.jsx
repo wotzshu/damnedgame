7df24841ffaf1217e76ce7e4b369f2e8c1f92025
wotzshu <wotzshuofficial@gmail.com>
2025-03-23 13:37:01 +0300

    Update Header.jsx

--- components/Static/Header.jsx
+++ components/Static/Header.jsx
@@ -168,7 +168,7 @@ const Header = ({ $, NavItems }) => {
                         </Menu>
                         <Link href="https://discord.com/api/oauth2/authorize?client_id=774043716797071371&permissions=277028620608&scope=applications.commands%20bot">
                             <a className="w-auto flex items-center justify-center shadow-lg gap-x-2 shadow-amber-600/20 rounded-xl py-2.5 font-medium px-7 bg-gradient-to-tl from-amber-500 to-amber-700 text-white  hover:opacity-80 transition duration-200">
-                               Invite
+                               Download Now
                             </a>
                </Link>
                     </div>
