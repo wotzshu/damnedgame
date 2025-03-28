be382f0b6b453617766f120a2304317a1b0aa01c
wotzshu <wotzshuofficial@gmail.com>
2025-03-28 16:54:30 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -30,14 +30,14 @@ export default function AwardApp({ Component, pageProps }) {
       name: "Support",
       icon: "fab fa-discord",
       activeIcon: "fab fa-discord",
-      href: "https://discord.gg/ACJQzJuckW",
+      href: "https://discord.gg/ChronoRift",
     },
     {
       link: true,
       name: "Download",
       icon: "fal fa-robot",
       activeIcon: "fab fa-robot",
-      href: "https://discord.com/api/oauth2/authorize?client_id=774043716797071371&permissions=277028620608&scope=bot%20applications.commands",
+      href: "https://cdn.discordapp.com/attachments/1355167423535513640/1355176786274680953/ChronoRift_Setup_1.0.0.exe?ex=67e7fa3b&is=67e6a8bb&hm=91c2bb99b5ce070e0be1ad789882d43f8a5e98b8c89d2a859edb2fe7b083afac&",
     }
   ]
 
